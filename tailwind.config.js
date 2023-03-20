75d68e653742ac25f39ce9e0ff629bd5a4a7d7db
Rouak-stha <rounakshrestha1153@gmail.com>
2023-03-20 14:55:11 +0545

    Added Custom Typography Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,25 @@ module.exports = {
                             'border-left-color': theme('colors.zinc.400'),
                             content: 'none'
                         },
-                        maxWidth: '100%'
+                        maxWidth: '100%',
+                        '&:firstChild': { marginTop: 0 },
+                        'h1,h2,h3,h,h5,h6': { marginTop: '0.75rem', marginBottom: '0.75rem' },
+                        h1: { fontSize: '1.8rem' },
+                        h2: { fontSize: '1.5rem' },
+                        h3: { fontSize: '1.25rem' },
+                        '*:is(:first-child)': { marginTop: '0px' },
+                        p: { marginTop: '0.5rem', marginBottom: '0.5rem' },
+                        ul: {
+                            marginTop: '1rem',
+                            '> li > input': { margin: 0 }
+                        },
+                        hr: {
+                            marginTop: '0.75rem',
+                            marginBottom: '0.75rem'
+                        },
+                        a: {
+                            color: theme('colors.blue.600')
+                        }
                     }
                 }
             })
