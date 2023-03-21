c214b6def453a3a7e700ecdb14336f0c0f420b9a
Rouak-stha <rounakshrestha1153@gmail.com>
2023-03-21 23:45:24 +0545

    Added Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,41 @@ export const metadata = {
     description: 'Create Your Short Notes'
 };
 
+const FooterData: { name: string; link: string }[] = [
+    {
+        name: 'GitHub',
+        link: 'https://github.com/Rounak-stha/snote'
+    },
+    {
+        name: 'MD_Editor',
+        link: 'https://github.com/rounak-stha/markdown-editor'
+    },
+    {
+        name: 'Developer',
+        link: 'https://www.rounakstha.me'
+    }
+];
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
             <body className={robotoMono.className}>
-                <div className='h-full xl:w-[800px] lg:w-2/3 sm:w-4/5 mx-auto pt-4 px-4 sm:px-8'>{children}</div>
+                <div className='h-screen flex flex-col xl:w-[800px] lg:w-2/3 sm:w-4/5 mx-auto pt-4 px-4 sm:px-8'>
+                    <div className='flex-1'>{children}</div>
+                    <div className='flex justify-around flex-wrap text-gray-400 my-4 pt-3 border-t-2 border-slate-700'>
+                        {FooterData.map(({ name, link }) => (
+                            <a
+                                key={name}
+                                className='hover:underline hover:text-blue-500'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                href={link}
+                            >
+                                {name}
+                            </a>
+                        ))}
+                    </div>
+                </div>
             </body>
         </html>
     );
